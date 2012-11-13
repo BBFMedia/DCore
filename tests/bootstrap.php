1529a54d62de342fcc1cf16d7ecabb34495370cd
Adrian Jones <adrianj1998@hotmail.com>
2012-11-13 10:08:11 -0800

    added test cases

---
+++ tests/bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * @author adrian
+ */
+// TODO: check include path
+ini_set('include_path', ini_get('include_path').PATH_SEPARATOR.dirname(__FILE__).'/../../../../Sandbox/Winginx/php5/pear');
+
+define('__SASS_PATH', dirname(dirname(__FILE__)).'/phpsass');
+
+// put your code here
+?>
